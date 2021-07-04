5bb79b8572d5942aed2a6b02ce20ccc296ba72cb
fmarcabru <onin@protonmail.com>
2021-07-05 09:05:56 +1000

    added fields to class

--- sipcall.py
+++ sipcall.py
@@ -1,10 +1,20 @@
 # define the class for sip call
 
 class sipCall:
-    def __init__(self, callid, from, to):
+    def __init__(self, callid, from, to, via, contact, allow, localrtpport, remotertpport, localrtpip, remotertpip, ssrc, rtpseqnumber, rtptime):
         self.callid = callid
         self.from = from
         self.to = to
+        self.via = via
+        self.contact = contact
+        self.allow = allow
+        self.localrtpport = localrtpport
+        self.remotertpport = remotertpport
+        self.localrtpip = localrtpip
+        self.remotertpip = remotertpip
+        self.ssrc = ssrc
+        self.rtpseqnumber = rtpseqnumber
+        self.rtptime = rtptime
 
     def sipTrying(self):
         self._trying = True
